f7220145a1f29a2546a75c8dd359e6ec4cca47a5
ChaconneLuo <asgardannila@gmail.com>
2023-05-18 18:29:17 +0800

    feat: WebGPU Test

---
+++ src/components/Test/Test.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+if (!navigator.gpu) {
+  console.log("WebGPU is not supported!");
+} else {
+  const init = async () => {
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter!.requestDevice();
+
+    const canvas = document.getElementById('webgpu') as HTMLCanvasElement;
+    const context = canvas!.getContext('webgpu') as GPUCanvasContext;
+
+    const format = navigator.gpu.getPreferredCanvasFormat();
+    context!.configure({
+      device: device,
+      format: format
+    })
+  }
+  init();
+}
+</script>
+
+<template>
+  <canvas id="webgpu" width="500" height="500"></canvas>
+</template>
+
+<style scoped></style>
---
+++ src/components/Vertex/vertex.wgsl
@@ -0,0 +1,6 @@
+@vertex
+fn main(@location(0) pos: vec3<f32>) -> @builtin(position) vec4<f32> {
+    var homogeneousPos = vec4<f32>(pos, 1.0);
+    homogeneousPos.x -= 0.2;
+    return homogeneousPos;
+}
