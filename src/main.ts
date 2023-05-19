c66c04696f8124f4fd0554f7430270a6d2ae5cd9
ChaconneLuo <asgardannila@gmail.com>
2023-05-19 12:29:18 +0800

    feat: use generate router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router';
+import { createRouter, createWebHistory } from 'vue-router';
 
+import routes from '~pages';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
 createApp(App).use(router).mount('#app')
