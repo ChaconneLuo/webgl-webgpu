a69cbd149ab1b6234f27958a49ef78b153b0a8fa
ChaconneLuo <asgardannila@gmail.com>
2023-05-25 16:41:49 +0800

    feat: UnoCSS

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router';
+import 'virtual:uno.css';
 
 import routes from '~pages';
 import extraRoute from './router';
