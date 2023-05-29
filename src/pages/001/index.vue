82056887da1f90d0e533c58b9e4b8c4def15f4ba
ChaconneLuo <asgardannila@gmail.com>
2023-05-29 18:39:33 +0800

    feat: init 008

--- src/pages/001/index.vue
+++ src/pages/001/index.vue
@@ -4,7 +4,6 @@ const gpuSupport = navigator.gpu ? true : false;
 
 <template>
   <div class="w-125 h-125 flex flex-row justify-center flex-items-center">
-
     <h3 v-if="gpuSupport">WebGPU Check:&nbsp;WebGPU is supported!</h3>
     <h1 v-else>WebGPU Check:&nbsp;WebGPU is not supported!</h1>
   </div>
---
+++ src/pages/008/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <canvas id="webgpu" width="800" height="600"></canvas>
+</template>
+
+<style scoped></style>
